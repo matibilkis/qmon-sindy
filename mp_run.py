a4f1fc6e612498a9f3e91587c6b9cb02222c40d5
mbilkis <mbilkis@pop-os.localdomain>
2023-10-26 20:22:33 +0200

    custom model works!

--- mp_run.py
+++ mp_run.py
@@ -1,15 +1,12 @@
 import os
 import multiprocessing as mp
 
-
 cores = mp.cpu_count()
 def int_seed(seed):
     for k in range(Nstep):
         os.system("python3 numerics/integration/integrate.py --itraj {}".format(itraj+k))
         print(f"{k}, {seed}, done")
-#
-#
+
 Nstep = cores-1
-# int_seed(1)
 with mp.Pool(cores-1) as p:
     p.map(int_seed, range(0,40, Nstep))
