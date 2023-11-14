3cfdd5cca57faaaa472a65ef0913671b331321fc
matibilkis <matiasbilkis@gmail.com>
2023-11-14 23:19:47 +0100

    running on cluster

--- .ipynb_checkpoints/mp_run-checkpoint.py
+++ .ipynb_checkpoints/mp_run-checkpoint.py
@@ -9,16 +9,11 @@ args = parser.parse_args()
 global itraj
 itraj = args.itraj
 
-cores = mp.cpu_count()
-print(cores)
-os.system("/data/jupyter/software/envs/master/bin/python3.11 numerics/integration/integrate.py --itraj {}".format(itraj))
-# def int_seed(seed):
-#     for k in range(Nstep):
-#         os.system("python3 numerics/integration/integrate.py --itraj {}".format(itraj+k))
-#         os.system("python3 numerics/NN/run_torch.py --itraj {}".format(itraj+k))
-#
-#         print(itraj, (datetime.now() - st).seconds)
-#
-# Nstep = cores-1
-# with mp.Pool(cores-1) as p:
-#     p.map(int_seed, range(0,40, Nstep))
+cores = 8
+python = "/data/jupyter/software/envs/master/bin/python3.11"
+def int_seed(seed):
+    os.system("{} numerics/integration/integrate.py --itraj {}".format(python,itraj+seed))
+    os.system("{} numerics/NN/run_torch.py --itraj {}".format(python,itraj+seed))
+
+with mp.Pool(cores) as p:
+    p.map(int_seed, range(0,cores))
