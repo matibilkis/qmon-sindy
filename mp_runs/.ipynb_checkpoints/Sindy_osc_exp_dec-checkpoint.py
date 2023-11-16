375726f1042da1142993c35faea7e960b3c0eae3
matibilkis <matiasbilkis@gmail.com>
2023-11-16 01:13:27 +0100

    some correction in the printing

--- mp_runs/.ipynb_checkpoints/Sindy_osc_exp_dec-checkpoint.py
+++ mp_runs/.ipynb_checkpoints/Sindy_osc_exp_dec-checkpoint.py
@@ -12,7 +12,7 @@ itraj = args.itraj
 python = "/data/jupyter/software/envs/master/bin/python3.11"
 def int_seed(itraj, seed=0):
     os.system("{} numerics/integration/external_forces/osc-exp-dec.py --itraj {}".format(python,itraj+seed))
-    os.system("{} numerics/NN/modes/Sindy_osc_exp_dec.py --itraj {} --printing 1".format(python,itraj+seed))
+    os.system("{} numerics/NN/modes/Sindy_osc_exp_dec.py --itraj {} --printing 0".format(python,itraj+seed))
 
 # with mp.Pool(cores) as p:
 #     p.map(int_seed, range(0,cores))
